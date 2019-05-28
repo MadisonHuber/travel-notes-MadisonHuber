a763b579bcdf247a8a4a35e9bf79af72892ec646
Madison Huber <hubermm@live.unc.edu>
2019-05-28 21:50:45 +0200

    Add sdd some pictures

--- src/script/Data.ts
+++ src/script/Data.ts
@@ -15,4 +15,24 @@ export default [
         "thumbSrc": "./img/google-thumb.jpg",
         "src": "./img/google.jpg",
     },
+    {
+        "thumbSrc": "./img/romerberg-thumb.jpg",
+        "src": "./img/romerberg.jpg",
+    },
+    {
+        "thumbSrc": "./img/f-h-thumb.jpg",
+        "src": "./img/f-h.jpg",
+    },
+    {
+        "thumbSrc": "./img/n-c-thumb.jpg",
+        "src": "./img/n-c.jpg",
+    },
+    {
+        "thumbSrc": "./img/garden-thumb.jpg",
+        "src": "./img/garden.jpg",
+    },
+    {
+        "thumbSrc": "./img/prague-thumb.jpg",
+        "src": "./img/prague.jpg",
+    },
 ];
